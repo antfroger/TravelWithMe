c00576dee40422d0d9251a7319d8cbb8343b7f3b
antoine <antfroger@gmail.com>
2013-06-29 13:06:11 +0200

    First step of the openshift deployment script

--- web/app.php
+++ web/app.php
@@ -3,12 +3,12 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
 /*
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
 */
@@ -16,6 +16,7 @@ $loader->register(true);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
+// Gets the configuration from the openshift environment variables
 $_SERVER['SYMFONY__DATABASE__HOST'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $_SERVER['SYMFONY__DATABASE__PORT'] = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $_SERVER['SYMFONY__DATABASE__USER'] = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
