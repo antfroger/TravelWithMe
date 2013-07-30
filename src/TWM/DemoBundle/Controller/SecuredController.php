311f3a5f9bd87cfc84204f64bcd277d5743d25c0
antoine <antfroger@gmail.com>
2013-07-30 21:01:08 +0200

    Prefix all routes with /demo Externalize the routing of SecuredController in the file secured.yml Remove the first _ of all routes

--- src/TWM/DemoBundle/Controller/SecuredController.php
+++ src/TWM/DemoBundle/Controller/SecuredController.php
@@ -2,19 +2,14 @@
 
 namespace TWM\DemoBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-/**
- * @Route("/twm/demo/secured")
- */
 class SecuredController extends Controller
 {
     /**
-     * @Route("/login", name="_twm_demo_login")
      * @Template()
      */
     public function loginAction(Request $request)
@@ -31,25 +26,17 @@ class SecuredController extends Controller
         );
     }
 
-    /**
-     * @Route("/login_check", name="_twm_security_check")
-     */
     public function securityCheckAction()
     {
         // The security layer will intercept this request
     }
 
-    /**
-     * @Route("/logout", name="_twm_demo_logout")
-     */
     public function logoutAction()
     {
         // The security layer will intercept this request
     }
 
     /**
-     * @Route("/hello", defaults={"name"="World"}),
-     * @Route("/hello/{name}", name="_twm_demo_secured_hello")
      * @Template()
      */
     public function helloAction($name)
@@ -58,7 +45,6 @@ class SecuredController extends Controller
     }
 
     /**
-     * @Route("/hello/admin/{name}", name="_twm_demo_secured_hello_admin")
      * @Template()
      */
     public function helloadminAction($name)
