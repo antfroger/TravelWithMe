4254ca891a60f097150580b5a5cccc26df42b953
antoine <antfroger@gmail.com>
2013-08-31 09:33:44 +0200

    Fix prod configuration

--- web/app.php
+++ web/app.php
@@ -3,12 +3,12 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
 /*
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
 */
