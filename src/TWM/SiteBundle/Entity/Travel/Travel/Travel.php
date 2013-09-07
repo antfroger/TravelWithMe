f5a0c202077d042841527b1a82f44efed65e9d2b
antoine <antfroger@gmail.com>
2013-09-07 18:53:26 +0200

    [#36] Add Copyright

--- src/TWM/SiteBundle/Entity/Travel/Travel/Travel.php
+++ src/TWM/SiteBundle/Entity/Travel/Travel/Travel.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Travel With Me
+ *
+ * @copyright (c) Antoine Froger <antfroger@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace TWM\SiteBundle\Entity\Travel\Travel;
 
 use DateTime;
